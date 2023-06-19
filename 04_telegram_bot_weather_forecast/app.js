45c83ad64c50e213f703d28f711d7beb2e02534a
markhryshchenko <grishenko.ua@gmail.com>
2023-06-19 17:01:29 +0300

    ready 04_task

---
+++ 04_telegram_bot_weather_forecast/app.js
@@ -0,0 +1,112 @@
+const axios = require("axios");
+const TelegramApi = require("node-telegram-bot-api");
+const token = "6148812560:AAGFCP95qD-L8tvBSOUWd5wp668hv96BDoo";
+const URL = "https://api.openweathermap.org/data/2.5/forecast?";
+const currentCity = "Харків";
+const userKey = "eb2ae53265b164308bbc5216022744c2";
+const bot = new TelegramApi(token, { polling: true });
+
+const getWeather = async (dataInterval, chatId) => {
+  try {
+    const response = await axios.get(`${URL}`, {
+      params: {
+        q: currentCity,
+        appid: userKey,
+        units: "metric",
+        lang: "ua",
+        //cnt: 6,
+      },
+    });
+
+    if (dataInterval == 3) {
+      for (item of response.data.list) {
+        let { name, main, weather, dt_txt } = item;
+        let tempCurrent = Math.round(main.temp);
+        let tempFeels = Math.round(main.feels_like);
+        bot.sendMessage(
+          chatId,
+          `Погода в ${currentCity}, ${dt_txt} температура: ${
+            tempCurrent > 0
+              ? "+" + tempCurrent
+              : tempCurrent < 0
+              ? "-" + tempCurrent
+              : tempCurrent
+          } відчувається: ${
+            tempFeels > 0
+              ? "+" + tempFeels
+              : tempFeels < 0
+              ? "-" + tempFeels
+              : tempFeels
+          }, ${weather[0].description} `
+        );
+      }
+    } else if (dataInterval == 6) {
+      for (let i = 0; i < response.data.list.length; i++) {
+        if (i % 2 == 0) {
+          let { name, main, weather, dt_txt } = response.data.list[i];
+          let tempCurrent = Math.round(main.temp);
+          let tempFeels = Math.round(main.feels_like);
+          bot.sendMessage(
+            chatId,
+            `Погода в ${currentCity}, ${dt_txt} температура: ${
+              tempCurrent > 0
+                ? "+" + tempCurrent
+                : tempCurrent < 0
+                ? "-" + tempCurrent
+                : tempCurrent
+            } відчувається: ${
+              tempFeels > 0
+                ? "+" + tempFeels
+                : tempFeels < 0
+                ? "-" + tempFeels
+                : tempFeels
+            }, ${weather[0].description} `
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Something wrong...", error);
+  }
+};
+
+const buttonsOptionsCity = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [[{ text: "Погода в Харкові", callback_data: "Харків" }]],
+  }),
+};
+const buttonsOptionsInterval = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        { text: "3 години", callback_data: "3" },
+        { text: "6 годин", callback_data: "6" },
+      ],
+    ],
+  }),
+};
+
+const start = () => {
+  bot.on("message", async (msg) => {
+    let text = msg.text;
+    let chatId = msg.chat.id;
+    if (text === "/start") {
+      return await bot.sendMessage(
+        chatId,
+        `Ласкаво просимо! Виберіть місто`,
+        buttonsOptionsCity
+      );
+    }
+    return bot.sendMessage(chatId, "Така команда відсутня");
+  });
+  bot.on("callback_query", (msg) => {
+    const chatId = msg.message.chat.id;
+    bot.sendMessage(chatId, `Вибери інтервал: `, buttonsOptionsInterval);
+    bot.on("callback_query", (msg) => {
+      const chatId = msg.message.chat.id;
+      const data = msg.data;
+      getWeather(data, chatId);
+    });
+  });
+};
+start();
